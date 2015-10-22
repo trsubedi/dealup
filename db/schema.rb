1bb80542a81745e92f646dc63be3d77dafacfae0
shiva subedi <trsubedi@hotmail.com>
2015-10-22 14:57:41 -0700

    added migrations and ffaker g

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022191003) do
+ActiveRecord::Schema.define(version: 20151022215549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20151022191003) do
     t.string   "condition"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,6 +37,7 @@ ActiveRecord::Schema.define(version: 20151022191003) do
     t.float    "lang"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "email"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'ffaker'
+20.times do ||
+  User.create(first_name: FFaker::Name::first_name, last_name: FFaker::Name::last_name, email: FFaker::Internet::email, password_digest: "password", location: FFaker::Address.city)
+end
+
+condition = ["Excellent", "good", "bad", "fucntional", "slightly damaged"]
+
+20.times do ||
+  Item.create(title: FFaker::Product.product, price: rand(1..1000), description: FFaker::Lorem.sentence, condition: condition[rand(condition.length)])
+end
